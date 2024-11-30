288cc533be265d8a202399a01326a16cc116a5c2
Lakatos Bence <lakatos816@gmail.com>
2024-11-30 15:33:15 +0100

    1. Two Sum - C#

---
+++ C#/C#/Program.cs
@@ -0,0 +1,8 @@
+﻿using C_.Problems.Easy;
+
+var problem = new Problem1_TwoSum();
+var nums = new int[] { 2, 7, 11, 15 };
+var target = 9;
+var result = problem.TwoSum(nums, target);
+
+Console.WriteLine(result);

5b9225f0aa92a4d90e6b2c55c51e7a0e2cf3c2f1
Lakatos Bence <lakatos816@gmail.com>
2024-12-15 12:53:00 +0100

    9. Palindrome Number - C#

---
+++ C#/LeetCode/Problems/Easy/Problem9_PalindromeNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    internal class Problem9_PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            int result = 0;
+            int xCopy = x;
+
+            while (xCopy > 0)
+            {
+                result *= 10;
+                result += xCopy % 10;
+                xCopy /= 10;
+            }
+
+            return result == x;
+        }
+    }
+}
---
+++ C#/LeetCode/Program.cs
@@ -0,0 +1,5 @@
+﻿using LeetCode.Problems.Easy;
+
+var problem = new Problem9_PalindromeNumber();
+
+Console.WriteLine(problem.IsPalindrome(121));

5cb510f8e516691f0ccef8e7ef1ee7d93f31b2ba
Lakatos Bence <lakatos816@gmail.com>
2025-03-13 14:58:25 +0100

    21. Merge Two Sorted Lists - C#

---
+++ C#/Problems/Easy/Problem21_MergeTwoSortedLists.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.Problems.Easy
+{
+    internal static class Problem21_MergeTwoSortedLists
+    {
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            var dummy = new ListNode();
+            var current = dummy;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val < list2.val)
+                {
+                    current.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    current.next = list2;
+                    list2 = list2.next;
+                }
+
+                current = current.next;
+            }
+
+            if (list1 != null)
+            {
+                current.next = list1;
+            }
+            else
+            {
+                current.next = list2;
+            }
+
+            return dummy.next;
+        }
+    }
+}

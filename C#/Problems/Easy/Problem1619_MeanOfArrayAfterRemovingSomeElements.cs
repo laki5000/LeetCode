72b2cb4aae41c10ce76a46be4a6b19a5b6ef4989
Lakatos Bence <lakatos816@gmail.com>
2025-03-17 15:01:19 +0100

    1619. Mean of Array After Removing Some Elements - C#

---
+++ C#/Problems/Easy/Problem1619_MeanOfArrayAfterRemovingSomeElements.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    internal static class Problem1619_MeanOfArrayAfterRemovingSomeElements
+    {
+        public static double TrimMean(int[] arr)
+        {
+            int elementsToRemove = (int)Math.Ceiling(arr.Length * 0.05);
+            Array.Sort(arr);
+            var trimmedArray = arr.Skip(elementsToRemove).Take(arr.Length - 2 * elementsToRemove);
+            return trimmedArray.Average();
+        }
+    }
+}

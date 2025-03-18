bac7daa27aa6f8c29d743e1ff6c17903965c13c6
Lakatos Bence <lakatos816@gmail.com>
2025-03-18 15:15:11 +0100

    1030. Matrix Cells in Distance Order - C#

---
+++ C#/Problems/Easy/Problem1030_MatrixCellsInDistanceOrder.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    internal static class Problem1030_MatrixCellsInDistanceOrder
+    {
+        public static int[][] AllCellsDistOrder(int rows, int cols, int rCenter, int cCenter)
+        {
+            var rowsXcols = rows * cols;
+            var matrix = new int[rowsXcols][];
+
+            for (int i = 0; i < rowsXcols; i++)
+            {
+                var currentRow = i / cols;
+                var currentCol = i % cols;
+
+                matrix[i] = [currentRow, currentCol];
+            }
+
+            Array.Sort(matrix, (a, b) =>
+                Math.Abs(a[0] - rCenter) + Math.Abs(a[1] - cCenter) -
+                (Math.Abs(b[0] - rCenter) + Math.Abs(b[1] - cCenter))
+            );
+
+            return matrix;
+        }
+    }
+}
--- C#/Program.cs
+++ C#/Program.cs
@@ -1,4 +1,4 @@
 ﻿using LeetCode.Problems.Easy;
 
 
-Console.WriteLine(Problem1619_MeanOfArrayAfterRemovingSomeElements.TrimMean([6, 0, 7, 0, 7, 5, 7, 8, 3, 4, 0, 7, 8, 1, 6, 8, 1, 1, 2, 4, 8, 1, 9, 5, 4, 3, 8, 5, 10, 8, 6, 6, 1, 0, 6, 10, 8, 2, 3, 4]));
+Console.WriteLine(Problem1030_MatrixCellsInDistanceOrder.AllCellsDistOrder(2,3,1,2));

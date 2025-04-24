598fbd319434ec7421654c4f9f25987b7ea5cb38
Lakatos Bence <lakatos816@gmail.com>
2025-04-24 14:29:23 +0200

    2331. Evaluate Boolean Binary Tree - C#

---
+++ C#/Problems/Easy/Problem2331_EvaluateBooleanBinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Shared;
+
+namespace LeetCode.Problems.Easy
+{
+    internal static class Problem2331_EvaluateBooleanBinaryTree
+    {
+        public static bool EvaluateTree(TreeNode root)
+        {
+            switch(root.val)
+            {
+                case 0:
+                    return false;
+                case 1:
+                    return true;
+                case 2:
+                    return EvaluateTree(root.left) || EvaluateTree(root.right);
+                case 3:
+                    return EvaluateTree(root.left) && EvaluateTree(root.right);
+                default:
+                    return false;
+            }
+        }
+    }
+}
